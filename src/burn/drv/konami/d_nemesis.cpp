8deb1d0f08c0901362d81b3d9c0abb8627f50758
alhumbra <164629245+alhumbra@users.noreply.github.com>
2025-08-13 12:39:08 +0900

    Update d_nemesis.cpp
    opps

--- src/burn/drv/konami/d_nemesis.cpp
+++ src/burn/drv/konami/d_nemesis.cpp
@@ -4016,7 +4016,7 @@ struct BurnDriver BurnDrvNemesis = {
 	"Nemesis (North America Prototype Version) GRADIUS ORIGINS Hack\0", NULL, "Konami", "GX400",
 	NULL, NULL, NULL, NULL,
 	BDF_GAME_WORKING | BDF_CLONE | BDF_HACK  | BDF_HISCORE_SUPPORTED, 2, HARDWARE_KONAMI_68K_Z80, GBF_HORSHOOT, 0,
-	NULL, nemesisRomInfo, nemesisRomName, NULL, NULL, NULL, NULL, NemesisInputInfo, NemesisDIPInfo,
+	NULL, nemesisRompInfo, nemesispRomName, NULL, NULL, NULL, NULL, NemesisInputInfo, NemesisDIPInfo,
 	NemesisInit, DrvExit, NemesisFrame, DrvDraw, DrvScan, &DrvRecalc, 0x800,
 	256, 224, 4, 3
 };
@@ -4635,3 +4635,4 @@ struct BurnDriver BurnDrvTwinbeeb = {
 	TwinbeebInit, DrvExit, Gx400Frame, DrvDraw, DrvScan, &DrvRecalc, 0x800,
 	224, 256, 3, 4
 };
+
